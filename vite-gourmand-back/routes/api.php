38ac5d1ac297ec2af848ecae10956868f536e9dc
Nade04 <86410129+Nade0478@users.noreply.github.com>
2026-04-06 18:59:50 +0200

    modif nouvelle base de donnée vers phpMyAdmin

--- vite-gourmand-back/routes/api.php
+++ vite-gourmand-back/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\{
@@ -20,14 +19,15 @@ use App\Http\Controllers\API\{
 // Routes publiques (auth)
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
 
 // Routes protégées par JWT
 Route::middleware('auth:api')->group(function () {
 
+    // Auth
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Ressources protégées
     Route::apiResource('roles', RoleController::class);
     Route::apiResource('users', UserController::class);
     Route::apiResource('commandes', CommandeController::class);
@@ -38,10 +38,4 @@ Route::middleware('auth:api')->group(function () {
     Route::apiResource('themes', ThemeController::class);
     Route::apiResource('horaires', HoraireController::class);
     Route::apiResource('avis', AvisController::class);
-
-    // infos utilisateur connecté
-    Route::get('me', [AuthController::class, 'me']);
-
-    // déconnexion
-    Route::post('logout', [AuthController::class, 'logout']);
 });
--- vite-gourmand-back/tests/Feature/ExampleTest.php
+++ vite-gourmand-back/tests/Feature/ExampleTest.php
@@ -2,18 +2,24 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class UserTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_get_users_returns_success()
     {
-        $response = $this->get('/');
+        // 1. Créer un utilisateur
+        $user = User::factory()->create();
 
+        // 2. Générer un token JWT
+        $token = auth()->login($user);
+
+        // 3. Appeler la route protégée avec le token
+        $response = $this->withHeader('Authorization', "Bearer $token")
+            ->get('/api/users');
+
+        // 4. Vérifier que la route répond bien 200
         $response->assertStatus(200);
     }
 }
