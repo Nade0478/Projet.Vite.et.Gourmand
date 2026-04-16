fae0a5bc8d1be5e87fe8c3ed390ed27a1db274ea
Nade04 <86410129+Nade0478@users.noreply.github.com>
2026-04-16 05:12:16 +0200

    modif des fichier de déploiement

--- vite-gourmand-back/routes/api.php
+++ vite-gourmand-back/routes/api.php
@@ -21,21 +21,22 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 // Routes protégées par JWT
-Route::middleware('auth:api')->group(function () {
+// Route::middleware('auth:api')->group(function () {
 
-    // Auth
-    Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+// Auth
+Route::get('/me', [AuthController::class, 'me']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
-    // Ressources protégées
-    Route::apiResource('roles', RoleController::class);
-    Route::apiResource('users', UserController::class);
-    Route::apiResource('commandes', CommandeController::class);
-    Route::apiResource('menus', MenuController::class);
-    Route::apiResource('plats', PlatController::class);
-    Route::apiResource('allergenes', AllergeneController::class);
-    Route::apiResource('regimes', RegimeController::class);
-    Route::apiResource('themes', ThemeController::class);
-    Route::apiResource('horaires', HoraireController::class);
-    Route::apiResource('avis', AvisController::class);
-});
+// Ressources protégées
+Route::apiResource('roles', RoleController::class);
+Route::apiResource('users', UserController::class);
+Route::apiResource('commandes', CommandeController::class);
+Route::apiResource('menus', MenuController::class);
+Route::apiResource('plats', PlatController::class);
+Route::apiResource('allergenes', AllergeneController::class);
+Route::apiResource('regimes', RegimeController::class);
+Route::apiResource('themes', ThemeController::class);
+Route::apiResource('horaires', HoraireController::class);
+Route::apiResource('avis', AvisController::class);
+
+// });  
